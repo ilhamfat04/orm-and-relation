ce9ac2ae0c590a43a94230d131a14359435b3c4e
= <=>
2021-12-22 11:34:19 +0700

    update

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,9 +1,50 @@
 // import models here
+const { product, user } = require('../../models')
 
-exports.getProduct = async (req, res) => {
+exports.getProducts = async (req, res) => {
   // code here
+  try {
+    const data = await product.findAll({
+      include: {
+        model: user,
+        as: "user",
+        attributes: {
+          exclude: ['password', 'createdAt', 'updatedAt']
+        }
+      },
+      attributes: {
+        exclude: ['idUser', 'createdAt', 'updatedAt']
+      }
+    })
+
+    res.send({
+      status: "Success",
+      data
+    })
+  } catch (error) {
+    res.send({
+      status: "failed",
+      message: 'server error'
+    })
+  }
 };
 
 exports.addProduct = async (req, res) => {
   // code here
+  try {
+    const data = req.body
+
+    await product.create(data)
+
+    res.send({
+      status: "Success",
+      data
+    })
+
+  } catch (error) {
+    res.send({
+      status: "failed",
+      message: 'server error'
+    })
+  }
 };
