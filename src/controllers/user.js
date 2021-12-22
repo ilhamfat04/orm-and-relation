b570cef660f04d98da0e845a54147ead6d9dbe4b
= <=>
2021-12-22 09:34:44 +0700

    update

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,21 @@
 // import model here
+const { user } = require('../../models')
 
-exports.addUsers = async (req, res) => {
+exports.addUser = async (req, res) => {
     // code here
+    try {
+        const data = req.body
+        const createdData = await user.create(data)
+
+        res.send({
+            status: "success",
+            data: createdData
+        })
+
+    } catch (error) {
+        res.send({
+            status: "failed",
+            message: "server error"
+        })
+    }
 }
